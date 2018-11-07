f1a84562bc0f4c36b2304e586c513ee6d495b2b9
Brandon McFarland <branmcf@gmail.com>
2018-11-06 20:42:22 -0600

    repo setup

---
+++ README.md
@@ -0,0 +1,7 @@
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/8098163/48107142-f8b21600-e203-11e8-9788-07f027b2d412.png" alt="MSL A6 image"/>
+</p>
+
+<p align="center">
+  <img src="http://img.shields.io/Version/0.1.0.png" alt="Assignment 6 Version 0.1.0"/>
+</p>
---
+++ client/ImageLab/ImageLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageLab
+//
+//  Created by Eric Larson
+//  Copyright © 2016 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
