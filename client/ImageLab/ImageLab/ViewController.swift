1122ae61f76ef5e46cd9666f7366426bd5cc8507
Brandon McFarland <branmcf@gmail.com>
2018-11-09 13:11:36 -0600

    Added correct image lab branch

--- client/ImageLab/ImageLab/ViewController.swift
+++ client/ImageLab/ImageLab/ViewController.swift
@@ -7,14 +7,215 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController   {
 
+    //MARK: Class Properties
+    var filters : [CIFilter]! = nil
+    var videoManager:VideoAnalgesic! = nil
+    let pinchFilterIndex = 2
+    var detector:CIDetector! = nil
+    let bridge = OpenCVBridge()
+    
+    @IBOutlet weak var rightEye: UILabel!
+    @IBOutlet weak var leftEye: UILabel!
+    @IBOutlet weak var mouth: UILabel!
+    //MARK: Outlets in view
+    @IBOutlet weak var flashSlider: UISlider!
+    @IBOutlet weak var stageLabel: UILabel!
+    
+    @IBOutlet weak var flashButton: UIButton!
+    @IBOutlet weak var cameraButton: UIButton!
+    //MARK: ViewController Hierarchy
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = nil
+        self.setupFilters()
+        
+        self.bridge.loadHaarCascade(withFilename: "parojosG")
+        self.bridge.loadHaarCascade(withFilename: "Mouth")
+
+        self.bridge.processType = 1;
+        self.videoManager = VideoAnalgesic.sharedInstance
+        self.videoManager.setCameraPosition(position: AVCaptureDevice.Position.front)
+        
+        // create dictionary for face detection
+        // HINT: you need to manipulate these proerties for better face detection efficiency
+        let optsDetector = [CIDetectorAccuracy:CIDetectorAccuracyLow,CIDetectorTracking:true] as [String : Any]
+        
+        // setup a face detector in swift
+        self.detector = CIDetector(ofType: CIDetectorTypeFace,
+                                  context: self.videoManager.getCIContext(), // perform on the GPU is possible
+            options: (optsDetector as [String : AnyObject]))
+        
+        self.videoManager.setProcessingBlock(newProcessBlock: self.processImage)
+        
+        if !videoManager.isRunning{
+            videoManager.start()
+        }
+    
+    }
+    
+    func viewWillDisappear() {
+        if videoManager.isRunning {
+            videoManager.stop()
+        }
+    }
+    
+    //MARK: Process image output
+    
+    func processImage(inputImage:CIImage) -> CIImage{
+        
+        // detect faces
+        let f = getFaces(img: inputImage)
+        
+        // if no faces, just return original image
+        if f.count == 0 { return inputImage }
+        
+        var retImage = inputImage
+        
+      
+        self.bridge.setTransforms(self.videoManager.transform)
+        
+        
+        
+        for feature in f {
+            self.bridge.setImage(retImage,
+                                 withBounds: feature.bounds, // the first face bounds
+                andContext: self.videoManager.getCIContext())
+            self.bridge.processImage()
+            retImage = self.bridge.getImageComposite()
+            
+            if(self.bridge.processType == 2){
+                DispatchQueue.main.async {
+                    self.mouth.isHidden = true;
+                    self.rightEye.isHidden = true;
+                    self.leftEye.isHidden = true;
+                }
+            }  else {
+                DispatchQueue.main.async {
+
+                self.mouth.isHidden = false;
+                self.rightEye.isHidden = false;
+                self.leftEye.isHidden = false;
+                }
+                if (feature.hasSmile) {
+                    DispatchQueue.main.async {
+                        self.mouth.text = "Smiling"
+                    }
+                }
+                else {
+                    DispatchQueue.main.async {
+                        self.mouth.text = "Not Smiling"
+                    }
+                }
+                
+                if (feature.leftEyeClosed) {
+                    DispatchQueue.main.async {
+                        self.leftEye.text = "Closed"
+                    }
+                }
+                    
+                else {
+                    DispatchQueue.main.async {
+                        self.leftEye.text = "Open"
+                    }
+                }
+                if (feature.rightEyeClosed) {
+                    DispatchQueue.main.async {
+                        self.rightEye.text = "Closed"
+                    }
+                }
+                else {
+                    DispatchQueue.main.async {
+                    self.rightEye.text = "Open"
+                }
+            }
+            
+          
+            }
+        }
+        return retImage
+
+    }
+    
+    //MARK: Setup filtering
+    func setupFilters(){
+        filters = []
+        
+        let filterPinch = CIFilter(name:"CIBumpDistortion")!
+        filterPinch.setValue(-0.5, forKey: "inputScale")
+        filterPinch.setValue(75, forKey: "inputRadius")
+        filters.append(filterPinch)
+        
+    }
+    
+    //MARK: Apply filters and apply feature detectors
+    func applyFiltersToFaces(inputImage:CIImage,features:[CIFaceFeature])->CIImage{
+        var retImage = inputImage
+        var filterCenter = CGPoint()
+        
+        for f in features {
+            //set where to apply filter
+            filterCenter.x = f.bounds.midX
+            filterCenter.y = f.bounds.midY
+            
+            //do for each filter (assumes all filters have property, "inputCenter")
+            for filt in filters{
+                filt.setValue(retImage, forKey: kCIInputImageKey)
+                filt.setValue(CIVector(cgPoint: filterCenter), forKey: "inputCenter")
+                // could also manipualte the radius of the filter based on face size!
+                retImage = filt.outputImage!
+            }
+        }
+        return retImage
+    }
+    
+    func getFaces(img:CIImage) -> [CIFaceFeature]{
+        let optsFace = [CIDetectorImageOrientation:self.videoManager.ciOrientation, CIDetectorEyeBlink:true, CIDetectorSmile:true] as [String : Any]
+
+        return self.detector.features(in: img, options: optsFace) as! [CIFaceFeature]
+    }
+    
+    @IBAction func swipeRecognized(_ sender: UISwipeGestureRecognizer) {
+        switch sender.direction {
+        case UISwipeGestureRecognizerDirection.left:
+            print("Swiped left!")
+            self.bridge.processType += 1
+        case UISwipeGestureRecognizerDirection.right:
+            print("Swiped right!")
+            self.bridge.processType -= 1
+        default:
+            break
+        }
+        stageLabel.text = "Stage: \(self.bridge.processType)"
+    }
+    
+    //MARK: Convenience Methods for UI Flash and Camera Toggle
+    @IBAction func flash(_ sender: AnyObject) {
+        if(self.videoManager.toggleFlash()){
+            self.flashSlider.value = 1.0
+        }
+        else{
+            self.flashSlider.value = 0.0
+        }
+    }
+    
+    @IBAction func switchCamera(_ sender: AnyObject) {
+        self.videoManager.toggleCameraPosition()
+    }
+    
+    @IBAction func setFlashLevel(sender: UISlider) {
+        if(sender.value>0.0){
+            self.videoManager.turnOnFlashwithLevel(sender.value)
+        }
+        else if(sender.value==0.0){
+            self.videoManager.turnOffFlash()
+        }
     }
 
-
+   
 }
 
---
+++ client/imageLab/ImageLab/OpenCVBridgeSub.mm
@@ -0,0 +1,68 @@
+//
+//  OpenCVBridgeSub.m
+//  ImageLab
+//
+//  Created by Eric Larson on 10/4/16.
+//  Copyright © 2016 Eric Larson. All rights reserved.
+//
+
+#import "OpenCVBridgeSub.h"
+
+#import "AVFoundation/AVFoundation.h"
+
+
+using namespace cv;
+
+@interface OpenCVBridgeSub()
+@property (nonatomic) cv::Mat image;
+@end
+
+@implementation OpenCVBridgeSub
+@dynamic image;
+//@dynamic just tells the compiler that the getter and setter methods are implemented not by the class itself but somewhere else (like the superclass or will be provided at runtime).
+
+-(void)processImage{
+    NSLog(@"Inside processImage");
+    
+    cv::Mat frame_gray,image_copy;
+    char text[50];
+    char text2[50];
+    char text3[50];
+    float blue [100];
+    float green [100];
+    float red [100];
+    static int counter = 0;
+    Scalar avgPixelIntensity;
+    cv::Mat image = self.image;
+    cvtColor(image, image_copy, CV_BGRA2BGR); // get rid of alpha for processing
+    avgPixelIntensity = cv::mean( image_copy );
+    sprintf(text,"Avg. B: %.0f, G: %.0f, R: %.0f", avgPixelIntensity.val[2],avgPixelIntensity.val[1],avgPixelIntensity.val[0]);
+    cv::putText(image, text, cv::Point(50, 50), FONT_HERSHEY_PLAIN, 0.75, Scalar::all(255), 1, 2);
+//    check if there is something covering the camera that causes any one pixel intensity to dip below 100
+    if (avgPixelIntensity.val[2] < 100 || avgPixelIntensity.val[1] < 100 || avgPixelIntensity.val[0] < 100) {
+        sprintf(text2,"SOMETHING IS COVERING");
+        cv::putText(image,text2,cv::Point(100,100), FONT_HERSHEY_PLAIN, 0.75, Scalar::all(255), 1, 2);
+        self.isCovered = true;
+        if(counter == 99) {
+            sprintf(text3,"ARRAYS ARE FULL");
+            cv::putText(image,text3,cv::Point(100,200), FONT_HERSHEY_PLAIN, 0.75, Scalar::all(255), 1, 2);
+        }
+        else {
+            blue[counter] = avgPixelIntensity.val[2];
+            green[counter] = avgPixelIntensity.val[1];
+            red[counter] = avgPixelIntensity.val[0];
+            counter += 1;
+        }
+
+        
+    }
+    else {
+        self.isCovered = false;
+    }
+    
+    self.image = image;
+}
+
+
+
+@end
